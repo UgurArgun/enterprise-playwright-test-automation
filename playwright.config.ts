a1c3e925041a742576ac061693c0a61d070fc5a7
ugur <ugur@skenariolabs.com>
2025-11-04 01:17:13 +0200

    updated config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ if (!process.env.NODE_ENV) {
 }
 
 export default defineConfig({
-  testDir: './src/tests',
+  testDir: "./src/tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -31,38 +31,35 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-    baseURL: 'https://login.salesforce.com',
+    baseURL: "https://ui.cogmento.com",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-    
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-  use: { ...devices['Desktop Chrome'], headless: false },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"], headless: true },
     },
 
     {
-      name: 'firefox',
-  use: { ...devices['Desktop Firefox'], headless: false },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"], headless: true },
     },
 
     {
-      name: 'webkit',
-  use: { ...devices['Desktop Safari'], headless: false },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"], headless: true },
     },
-
-
 
     /* Test against mobile viewports. */
     // {
